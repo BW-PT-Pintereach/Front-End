e93712fd0e535f76c25b52e350ba0e449e04a43c
Mikey Williams <manicdesigner@gmail.com>
2019-11-16 20:48:43 -0500

    route added articles

--- front-end/src/components/Routers.js
+++ front-end/src/components/Routers.js
@@ -1,9 +1,14 @@
 import React from 'react'
-import {Route} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import LoginSignup from '../components/LoginSignup'
+import Articles from '../components/Articles'
 
-export default function(props) {
+export default function (props) {
     return (
-        <Route exact path='/' component={LoginSignup} />
+        <>
+            <Route exact path='/' component={LoginSignup} />
+            <Route path='/articles' component={Articles} />
+        </>
+
     )
 }
