fc8b844534bb20ea62b8bed7a92468b8ecba579f
Mikey Williams <manicdesigner@gmail.com>
2019-11-16 21:45:07 -0500

    add activeArticle state

--- front-end/src/components/Articles.js
+++ front-end/src/components/Articles.js
@@ -5,19 +5,29 @@ import ArticleCard from './ArticleCard';
 function Articles(props) {
     const [getArticles, setGetArticles] = useState([]);
 
+    const [activeArticles, setActiveArticles] = useState([]);
+
+    const retrieveActiveArticles = (query) => {
+        query.toLowerCase() === 'all' ? setActiveArticles(getArticles) : setActiveArticles(getArticles.filter(article => article.category_name.toLowerCase() === query.toLowerCase()))
+    }
+
     useEffect(() => {
         axiosWithAuth().get('articles')
             .then(result => {
                 console.log(result);
-                setGetArticles(result.data);
+                [setGetArticles, setActiveArticles].forEach(cb => cb(result.data.filter(article => `${article.user_id}` === `${getId()}`
+                )));
+
             })
             .catch(error => {
                 console.log(error);
             })
     }, [])
+
+
     return (
         <>
-            {getArticles.length > 0 && getArticles.map(article => <ArticleCard article={article} />)}
+            {activeArticles.length > 0 && activeArticles.map(article => <ArticleCard key={article.id} article={article} />)}
         </>
     )
 }
