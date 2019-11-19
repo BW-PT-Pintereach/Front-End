ac6049f5aefb28e26c91535ae907dd96cce4dbe3
Joshua-Burleson <joshua.burleson.developer@gmail.com>
2019-11-18 20:30:06 -0500

    Private route, and form validation added

---
+++ front-end/src/components/PrivateRoute.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Redirect, Route} from 'react-router-dom';
+
+const PrivateRoute = ({component: Component, ...rest}) => 
+     <Route {...rest} render={props => localStorage.getItem('token') !== null ? (<Component {...props} />) : (<Redirect to='/' />)} />
+
+export default PrivateRoute;
