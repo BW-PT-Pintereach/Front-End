34ccd394d132af026a9e98e9c2816fd2920fead8
Mikey Williams <manicdesigner@gmail.com>
2019-11-20 22:04:47 -0500

    add article prop to modal

--- front-end/src/components/Modal.js
+++ front-end/src/components/Modal.js
@@ -1,12 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import FormikAddEdit from './AddEdit'
 
 const Modal = (props) => {
+    const article = props.location.article;
     const method = props.match.params.method;
     return (
         <div>
-            <FormikAddEdit method={`${method[0].toUpperCase()}${method.slice(1)}`} />
+            <FormikAddEdit article={article} method={`${method[0].toUpperCase()}${method.slice(1)}`} />
         </div>
     )
 }
